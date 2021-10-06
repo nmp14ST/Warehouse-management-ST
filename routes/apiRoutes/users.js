347ed276587f54da6286760e5dbd947901acbd45
Nathan Peek <nmp14fsu@gmail.com>
2021-10-06 10:37:51 -0400

    Add register route

--- routes/apiRoutes/users.js
+++ routes/apiRoutes/users.js
@@ -1,13 +1,25 @@
-const login = require("../../controllers/users");
+const { login, register } = require("../../controllers/users");
 
 const router = require("express").Router();
 
 router.post("/login", async (req, res) => {
     try {
-        const { user } = await login(req.body.email, req.body.password);
-        console.log(user);
+        const user = await login(req.body.email, req.body.password);
 
-        res.status(200).json({ user });
+        res.status(200).json(user);
+    } catch (err) {
+        res.status(err.status ? err.status : 500).json(err);
+        console.error(err);
+    }
+});
+
+router.post("/register", async (req, res) => {
+    try {
+        const user = await register(req.body.firstName, req.body.lastName, req.body.email, req.body.password, req.body.company, req.body.access);
+
+        if (user.id) {
+            res.status(201).json(user);
+        }
     } catch (err) {
         res.status(err.status ? err.status : 500).json(err);
         console.error(err);
