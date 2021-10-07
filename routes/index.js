4502d71087ebbb5e86d909e94cc3850db6bc3369
Nathan Peek <nmp14fsu@gmail.com>
2021-10-07 11:59:38 -0400

    Fix ordering of routes so * in viewRoutes does not grab apiroutes

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ const router = require("express").Router();
 const viewRoutes = require("./viewRoutes");
 const apiRoutes = require("./apiRoutes");
 
-router.use("/", viewRoutes);
 router.use("/api", apiRoutes);
+router.use("/", viewRoutes);
 
 module.exports = router;
