f13c537c95b4c32bd75deda0ac55382fd0963be6
Nathan Peek <nmp14fsu@gmail.com>
2021-10-07 10:03:00 -0400

    Create access lvl middleware

--- utils/index.js
+++ utils/index.js
@@ -12,4 +12,48 @@ const generateToken = user => {
     });
 }
 
-module.exports = { generateToken };
+const isAuth = (req, res, next) => {
+    const authorization = req.headers.authorization;
+    if (authorization) {
+        const token = authorization.slice(7, authorization.length); // Bearer XXXXX
+        jwt.verify(token, process.env.JWT_SECRET, (err, decode) => {
+            if (err) {
+                res.status(401).json({ message: "Invalid token" });
+            } else {
+                req.user = decode;
+                next();
+            }
+        });
+    } else {
+        res.status(401).json({ message: "No Token" });
+    }
+}
+
+// Top lvl auth
+const isManager = (req, res, next) => {
+    if (req.user && req.user.access === 1) {
+        next();
+    } else {
+        res.status(401).json({ message: "Insufficient access rights" });
+    }
+}
+
+// Lvl 2 auth (2nd highest of 4)
+const isAdmin = (req, res, next) => {
+    if (req.user && req.user.access <= 2) {
+        next();
+    } else {
+        res.status(401).json({ message: "Insufficient access rights" });
+    }
+}
+
+// Lvl 3 auth (3rd highest of 4)
+const isUpdater = (req, res, next) => {
+    if (req.user && req.user.access <= 3) {
+        next();
+    } else {
+        res.status(401).json({ message: "Insufficient access rights" });
+    }
+}
+
+module.exports = { generateToken, isAuth, isManager, isAdmin, isUpdater };
