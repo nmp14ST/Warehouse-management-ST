220cd7aed8ef5d8241204ed76c392430dbcfb09c
Nathan Peek <nmp14fsu@gmail.com>
2021-10-08 09:45:45 -0400

    Add left nav bar with open and closing functionality

--- public/js/index.js
+++ public/js/index.js
@@ -1,4 +1,26 @@
+const navExpand = document.querySelector(".nav__expand");
+const nav = document.querySelector(".left-nav");
+const navListItem = document.querySelectorAll(".nav-list-item");
+
 // Redirect if not logged in
 if (!localStorage.getItem("user")) {
     document.location.href = "/login";
 }
+
+// Expand and close left-nav bar
+navExpand.addEventListener("click", () => {
+    nav.classList.toggle("closed");
+});
+
+//  Change active list item in left nav bar
+function listActive(e) {
+    navListItem.forEach(link => {
+        link.classList.remove("nav-list-item-active");
+        this.classList.add("nav-list-item-active");
+    });
+}
+
+//  Event listener for each list item
+navListItem.forEach(link => {
+    link.addEventListener("click", listActive);
+});
