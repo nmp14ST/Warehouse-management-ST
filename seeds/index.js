17c9eec1b9008c5a7a7fea17bcdead2bc430e103
Nathan Peek <nmp14fsu@gmail.com>
2021-10-08 20:40:54 -0400

    Create seeds for businesses

--- seeds/index.js
+++ seeds/index.js
@@ -1,9 +1,15 @@
 const mongoose = require("mongoose");
 const companySeeds = require("./companies.json");
 const db = require("../models");
+require("dotenv").config();
 
 const seed = async () => {
     await mongoose.connect(process.env.mongo_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+
+    const business = new db.Business(companySeeds);
+
+    await business.save();
+    console.log("\n\n Business seeds run \n\n");
 
     mongoose.connection.close();
 }
