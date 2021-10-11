e354abb797d3c8306a3d86163c32d79c7fae2ac5
Nathan Peek <nmp14fsu@gmail.com>
2021-10-11 15:09:02 -0400

    Add warehosue rotue for getting all warehouses

--- routes/apiRoutes/warehouse.js
+++ routes/apiRoutes/warehouse.js
@@ -1,5 +1,20 @@
 const router = require("express").Router();
+const mongoose = require("mongoose");
+const db = require("../../models");
 
+router.get("/", async (req, res) => {
+    try {
+        await mongoose.connect(process.env.mongo_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
+        const warehouses = await db.Warehouse.find({});
+
+        res.status(200).json(warehouses);
+        mongoose.connection.close();
+    } catch (err) {
+        mongoose.connection.close();
+        res.status(err.status ?? 500).json(err);
+        console.error(err);
+    }
+});
 
 module.exports = router;
