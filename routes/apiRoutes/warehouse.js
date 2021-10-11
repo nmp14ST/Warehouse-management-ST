a7f52359678b74e2090b2773d60f20aeb3ac63f0
Nathan Peek <nmp14fsu@gmail.com>
2021-10-11 17:10:27 -0400

    Create route for getitng certain warehouses for company

--- routes/apiRoutes/warehouse.js
+++ routes/apiRoutes/warehouse.js
@@ -17,4 +17,38 @@ router.get("/", async (req, res) => {
     }
 });
 
+// Route to get all warehouses for specific company
+router.get("/:name", async (req, res) => {
+    try {
+        await mongoose.connect(process.env.mongo_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+
+        const warehouse = await db.Warehouse.find({ company_name: req.params.name });
+
+        console.log(req.params.name);
+
+        if (!warehouse) {
+            throw { status: 404, message: "Cannot find that warehouse" };
+        }
+
+        res.status(200).json(warehouse);
+        mongoose.connection.close();
+    } catch (err) {
+        mongoose.connection.close();
+        res.status(err.status ?? 500).json(err);
+        console.error(err);
+    }
+});
+
+router.get("/single/:id", async (req, res) => {
+    try {
+        await mongoose.connect(process.env.mongo_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+
+        mongoose.connection.close();
+    } catch (err) {
+        mongoose.connection.close();
+        res.status(err.status ?? 500).json(err);
+        console.error(err);
+    }
+});
+
 module.exports = router;
