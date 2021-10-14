572f0f806e654deb5a4a17a156954bdf4b3087d4
Nathan Peek <nmp14fsu@gmail.com>
2021-10-13 20:44:49 -0400

    Force seed to clear collections

--- seeds/index.js
+++ seeds/index.js
@@ -7,11 +7,13 @@ require("dotenv").config();
 const seed = async () => {
     await mongoose.connect(process.env.mongo_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
+    await db.Business.remove({});
     const business = new db.Business(companySeeds);
 
     await business.save();
     console.log("\n\n Business seeds run \n\n");
 
+    await db.Warehouse.remove({});
     const warehouses = await db.Warehouse.insertMany(warehouseSeeds)
         .then(() => console.log("\n\n warehouse seeds run"))
         .catch((err) => console.log("\n" + err + "\n"));
