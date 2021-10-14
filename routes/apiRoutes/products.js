0d4e21a157ee04a325f005fc684f64be16c1cece
Nathan Peek <nmp14fsu@gmail.com>
2021-10-13 20:46:52 -0400

    Create route for adding product to warehouse

--- routes/apiRoutes/products.js
+++ routes/apiRoutes/products.js
@@ -1,9 +1,21 @@
 const router = require("express").Router();
+const { addProductToWarehouse } = require("../../controllers/Products");
 
 router.get("/", (req, res) => {
     try {
         res.status(200).json({ data: "Some data" });
     } catch (err) {
+        console.error(err);
+    }
+});
+
+router.post("/:warehouseID", async (req, res) => {
+    try {
+        const whProduct = await addProductToWarehouse(req.body, req.params.warehouseID);
+
+        res.status(200).json(whProduct);
+    } catch (err) {
+        res.status(err.status ?? 500).json(err);
         console.error(err);
     }
 });
