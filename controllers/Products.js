871afecaeafe197411bb82579ee4013f901612da
Nathan Peek <nmp14fsu@gmail.com>
2021-10-14 13:51:32 -0400

    Create mongoose query for deleting product and remove form warehouse. Add comments

--- controllers/Products.js
+++ controllers/Products.js
@@ -3,6 +3,7 @@ require("dotenv").config();
 
 const db = require("../models");
 
+// Gets warehuse, checks if product can fit, creates product, adds product to wh, and increments warehouse size and numProducts
 const addProductToWarehouse = async ({ name, price, space, description }, whID) => {
     try {
         await mongoose.connect(process.env.mongo_URI, { useNewUrlParser: true, useUnifiedTopology: true });
@@ -21,8 +22,11 @@ const addProductToWarehouse = async ({ name, price, space, description }, whID) 
         const newProduct = new db.Product({ name, price, space, description });
         await newProduct.save();
 
+        // Add product to warehouse product array
         wh.products.push(newProduct._id);
+        // Inc number of products
         wh.numProducts++;
+        // Increase capacity of warehouse
         wh.size += parseInt(space);
 
         await wh.save();
@@ -35,4 +39,34 @@ const addProductToWarehouse = async ({ name, price, space, description }, whID) 
     }
 }
 
-module.exports = { addProductToWarehouse };
+// Find wh and pull product out, decrement size and numProducts, and delete product
+const deleteProductFromWarehouse = async (whID, pID) => {
+    try {
+        await mongoose.connect(process.env.mongo_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+
+        // Find, delete and return product
+        const deletedProduct = await db.Product.findOneAndDelete({ _id: pID });
+
+        // If product couldnt be found, throw 404
+        if (!deletedProduct) throw { stauts: 404, message: "Could not find product" }
+
+        // Find wh and remove the deleted products ID from products array and decrement the numProducts and size
+        const wh = await db.Warehouse.findOneAndUpdate(
+            {
+                _id: whID
+            }, {
+            $pull: { products: pID }, $inc: { numProducts: -1, size: -deletedProduct.space }
+        });
+
+        // No warehouse found throw 404
+        if (!wh) throw { status: 404, message: "Warehouse not found" };
+
+        mongoose.connection.close();
+        return deletedProduct;
+    } catch (err) {
+        mongoose.connection.close();
+        throw err;
+    }
+}
+
+module.exports = { addProductToWarehouse, deleteProductFromWarehouse };
