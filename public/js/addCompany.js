d09a260363ad2c093aefde4ce737de91230703bd
Nathan Peek <nmp14fsu@gmail.com>
2021-10-14 22:28:43 -0400

    Create fetch request to add new company

---
+++ public/js/addCompany.js
@@ -0,0 +1,37 @@
+// Get company name from query string
+const companyName = document.location.href.split("=")[1].trim();
+// Submiot form button
+const submitBtn = document.getElementById("submitBtn");
+
+const addCompanyFetch = async (e) => {
+    e.preventDefault();
+
+    const body = {
+        name: document.getElementById("name").value.trim(),
+        founded: document.getElementById("founded").value,
+        parent: companyName
+    }
+
+    const response = await fetch("/api/businesses", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(body)
+    });
+
+    const data = await response.json();
+
+    // Add error message if not successful
+    if (response.status !== 200) {
+        const p = document.createElement("p");
+        p.classList.add("text-center", "error");
+        p.textContent = data.message;
+
+        document.querySelector("form").appendChild(p);
+        return
+    }
+
+    // Redirect to app if success
+    document.location.href = "/interface";
+}
+
+submitBtn.addEventListener("click", addCompanyFetch);
